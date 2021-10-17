17e4fdc72ed2a21e986d61af858dfee9eac52a1e
andreisokhan <sokhanpageproofs@gmail.com>
2021-10-17 13:28:54 +0300

    2021/10/17_13-28_mob-first

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -2,8 +2,15 @@ function test(a, b) {
     return a + b
 }
 
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+    });
+});
 console.log(test(5, 2));
 console.log(test(2, 4));
 console.log(test(9, 1));
+
 
 //# sourceMappingURL=script.js.map
--- src/assets/js/lib.js
+++ src/assets/js/lib.js
@@ -1,3 +1,10 @@
 function test(a, b) {
     return a + b
 }
+
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+    });
+});
